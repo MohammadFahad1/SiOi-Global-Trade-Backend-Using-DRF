4071139c3407bbfb25ad91f6e2db4ad750e00777
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-11-15 22:48:54 +0600

    Demo apis created for products and categories

--- product/views.py
+++ product/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+@api_view()
+def view_products(request):
+    return Response({"message": "List of products"})
+
+@api_view()
+def view_categories(request):
+    return Response({"message": "List of categories"})
