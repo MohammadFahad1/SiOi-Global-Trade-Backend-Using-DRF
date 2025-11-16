e0bbaed6ca61b05d4ec058dc83dbfc55fd1d2e20
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-11-16 21:46:14 +0600

    Viewing a specific product using get_object_or_404 method in django

--- product/views.py
+++ product/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from product.models import Product, Category
 # Create your views here.
 @api_view()
-def view_products(request):
-    return Response({"message": "List of products"})
+def view_specific_products(request, id):
+        product = get_object_or_404(Product, pk=id)
+        product_dict = {"id": product.id, "name": product.name, "price": product.price}
+        return Response(product_dict)
 
 @api_view()
 def view_categories(request):
