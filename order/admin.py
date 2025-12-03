cd34f8369cc9234611a3085b3dd7ac392c8f91eb
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-12-03 19:48:05 +0600

    Cart Create and Retrive APIs implemented successfully

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from order.models import Cart, CartItem, Order, OrderItem
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'created_at', 'updated_at']
+
+# Register your models here.
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
 
 # Register your models here.
--- order/models.py
+++ order/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from users.models import User
 from product.models import Product
+from uuid import uuid4
 
 # Create your models here.
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart')
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.user.username + "'s Cart"
+        return self.user.first_name + "'s Cart"
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
@@ -18,8 +20,11 @@ class CartItem(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        unique_together = [['cart', 'product']]
+
     def __str__(self):
-        return f"{self.quantity} x {self.product.name} in {self.cart.user.username}'s Cart"
+        return f"{self.quantity} x {self.product.name} in {self.cart.user.first_name}'s Cart"
 
     def subtotal(self):
         return self.product.price * self.quantity
@@ -43,7 +48,7 @@ class Order(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Order #{self.id} by {self.user.username}"
+        return f"Order #{self.id} by {self.user.first_name}"
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
