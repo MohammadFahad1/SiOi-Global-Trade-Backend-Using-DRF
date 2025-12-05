fe1457458bead953af83ed589fda20c3d6569735
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-12-05 16:00:28 +0600

    CartItems are shown in the nested route

--- order/views.py
+++ order/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
-from order.serializers import CartSerializer
-from order.models import Cart
+from order.serializers import CartSerializer, CartItemSerializer
+from order.models import Cart, CartItem
 
 # Create your views here.
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
+
+class CartItemViewSet(ModelViewSet):
+    serializer_class = CartItemSerializer
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])
