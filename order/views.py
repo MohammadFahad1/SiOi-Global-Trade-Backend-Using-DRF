59477f2356fbf0f707bf67b54b611cf0c7c97a66
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-12-05 15:46:44 +0600

    Delete functionality added to the cart api

--- order/views.py
+++ order/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from rest_framework.viewsets import GenericViewSet
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from order.serializers import CartSerializer
 from order.models import Cart
 
 # Create your views here.
-class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
