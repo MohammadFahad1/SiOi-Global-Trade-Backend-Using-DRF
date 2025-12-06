a00ec1068f3702a96b518476924573e709c2f362
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-12-06 21:30:28 +0600

    Djoser and simpleJWT setup and registration api done

--- sioi_global_trade/urls.py
+++ sioi_global_trade/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('api.urls'), name='api-root'),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ] + debug_toolbar_urls()
