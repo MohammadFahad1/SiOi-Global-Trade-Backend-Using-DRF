2447d4761d4680ea5c5b9b4ff84a0fa6f8f57a39
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-12-16 20:07:35 +0600

    Custom Validator for validating product image size

---
+++ product/validators.py
@@ -0,0 +1,7 @@
+from django.core.exceptions import ValidationError
+
+def validate_file_size(file):
+    max_size_mb = 5
+    max_size_bytes = max_size_mb * 1024 * 1024
+    if file.size > max_size_bytes:
+        raise ValidationError(f"File size cannot exceed {max_size_mb} MB.")
